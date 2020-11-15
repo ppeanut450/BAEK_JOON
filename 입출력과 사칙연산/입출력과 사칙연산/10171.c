c722c52e9cf811055d5cdfd2d4cd67046216f33e
ppeanut450 <ppeanut450@gmail.com>
2020-11-15 23:03:16 +0900

    Q10171

---
+++ 입출력과 사칙연산/입출력과 사칙연산/10171.c
@@ -0,0 +1,13 @@
+/* 10171번
+아래 예제와 같이 고양이를 출력하시오. */
+
+#include <stdio.h>
+int main(void)
+{
+	printf("\\    /\\\n");
+	printf(" )  ( ')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|\n");
+
+	return 0;
+}
