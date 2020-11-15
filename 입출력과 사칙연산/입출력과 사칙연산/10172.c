8088324fbf99019cee0519a1eda81b5aed1d313f
ppeanut450 <ppeanut450@gmail.com>
2020-11-15 23:04:02 +0900

    Q10172

---
+++ 입출력과 사칙연산/입출력과 사칙연산/10172.c
@@ -0,0 +1,13 @@
+/* 10172번
+아래 예제와 같이 개를 출력하시오. */
+
+#include <stdio.h>
+int main(void)
+{
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\"^\"`    |\n");
+	printf("||_/=\\\\__| ");
+	return 0;
+}
