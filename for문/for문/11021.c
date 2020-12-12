ff0587b3917149dcad7cc9998694dbb5a770e028
ppeanut450 <ppeanut450@gmail.com>
2020-12-12 23:57:06 +0900

    Q11021

---
+++ for문/for문/11021.c
@@ -0,0 +1,21 @@
+/* 11021번
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오. */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	int A, B, T;
+	scanf("%d", &T);
+	int* sum = (int*)malloc(sizeof(int) * T);
+	for (int i = 0; i < T; i++)
+	{
+		scanf("%d %d", &A, &B);
+		sum[i] = A + B;
+	}
+	for (int i = 0; i < T; i++)
+		printf("Case #%d: %d\n", i + 1, sum[i]);
+
+	return 0;
+}
