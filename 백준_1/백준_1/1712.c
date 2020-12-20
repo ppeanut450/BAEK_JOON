01480d7cdd9c4d80b36e0ee894ec99d0c54299b2
ppeanut450 <ppeanut450@gmail.com>
2020-12-21 00:43:29 +0900

    Q1712

--- 백준_1/백준_1/1712.c
+++ 백준_1/백준_1/1712.c
@@ -2,21 +2,13 @@
 #include <stdio.h>
 int main(void)
 {
-	int A, B, C, i = 0;
+	int A, B, C;
 	scanf("%d %d %d", &A, &B, &C);
 
-	while (1)
-	{
-		if (i == B)
-			break;
-		i++;
-		if (i > A / (C - B))
-		{
-			printf("%d", i);
-			return 0;
-		}
-	}
-	if (-A - B * i + C * i <= 0)
+	if (B >= C)
 		printf("-1");
+	else
+		printf("%d", A / (C - B) + 1);
+
 	return 0;
 }
