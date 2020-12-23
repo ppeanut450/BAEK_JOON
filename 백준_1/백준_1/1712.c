a376d33b45f641324cd04cf5309eb34eaa10253d
ppeanut450 <ppeanut450@gmail.com>
2020-12-23 23:18:58 +0900

    Q1712

--- 백준_1/백준_1/1712.c
+++ 백준_1/백준_1/1712.c
@@ -4,20 +4,15 @@ int main(void)
 {
 	int A, B, C;
 	scanf("%d %d %d", &A, &B, &C);
-<<<<<<< HEAD
 
 	if (B >= C)
 		printf("-1");
 	else
 		printf("%d", A / (C - B) + 1);
-
-=======
 	
 	if (A / (C - B) + 1 > 0)
 		printf("%d", A / (C - B) + 1);
 	else
 		printf("-1");
-	
->>>>>>> 9a0705210200f2a69df04f8b4e55eca78e1f9e43
 	return 0;
 }
